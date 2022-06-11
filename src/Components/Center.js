27ee8ab5c3d397e741553a1a132184b0dc5dac32
Morvin-Ian <morvinian@gmail.com>
2022-06-11 11:48:26 +0300

    finalize

--- src/Components/Center.js
+++ src/Components/Center.js
@@ -1,14 +1,62 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { ExpandMoreOutlined, FavoriteOutlined, MoreVertOutlined, ShareLocation } from '@mui/icons-material';
+import { Avatar, Box, CardActionArea, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material'
+import { red } from '@mui/material/colors';
+import React from 'react';
+
+
 
 const Center = () => {
   return (
-    <Box bgcolor="red" p={3} flex={4}>
-        Center
+    <Box bgcolor="whitesmoke" p={3} flex={4}>
+
+    <CardActionArea sx={{ width: 700 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertOutlined />
+          </IconButton>
+        }
+        title="Morvin Ian"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="250"
+        src="https://images.unsplash.com/photo-1654881212180-86af196fd8dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteOutlined />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareLocation />
+        </IconButton>  
+          <ExpandMoreOutlined />
+      
+      </CardActions>
+    
+        <CardContent>
+          
+        </CardContent>
+  
+    </CardActionArea >
     </Box>
 
   ) 
     }
 
 
-export default Center
+export default Center;
--- src/Components/Rightbar.js
+++ src/Components/Rightbar.js
@@ -1,10 +1,20 @@
-import { Box } from '@mui/material'
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from '@mui/material'
 import React from 'react'
 
 const Rightbar = () => {
   return (
-    <Box bgcolor="blue" p={3} flex={2} sx={{display:{xs:"none", sm:"block"}}}>
-        Rightbar
+   
+   
+    <Box bgcolor="white" flex={2} sx={{display:{xs:"none", sm:"block"}}}>
+     
+      <AvatarGroup max={4}>
+ 
+        <Avatar alt="Remy Sharp" src="/static/images/avata/1.jpg" />
+        <Avatar alt="Travis Howard" src="/static/images/avaa/2.jpg" />
+        <Avatar alt="Cindy Baker" src="/static/images/avataa3.jpg" />
+        <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+        <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+      </AvatarGroup>
     </Box>
 
   )
